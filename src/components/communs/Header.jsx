641df96807954bf311678a143b895f79843ce89e
apple <apple@MacBook-Pro-2.local>
2025-07-21 11:30:38 +0300

    [UPDATE] les elements de l'header centrees et responsives

--- src/components/communs/Header.jsx
+++ src/components/communs/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
       <div className="container mx-auto px-4 py-4">
         <div className="flex items-center justify-between">
           <div className="flex items-center space-x-2">
-            <div className="w-10 h-10 bg-gradient-to-r from-red-500 to-blue-500 rounded-full flex items-center justify-center">
+            <div className="w-10 h-10 bg-gradient-to-r from-red-500 to-blue-500 rounded-full flex items-center justify-center ml-11">
               <Zap className="w-6 h-6 text-white" />
             </div>
             <h1 className="text-2xl font-bold bg-gradient-to-r from-red-600 to-blue-600 bg-clip-text text-transparent">
