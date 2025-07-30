d95c74d3f35dd3495b50f8a754ec7ec53d691377
apple <apple@MacBook-Pro-2.local>
2025-07-30 19:02:39 +0300

    [Update] Ajout des petit transition

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,4 +1,4 @@
-import Header from '../components/communs/Header';
+
 import Footer from '../components/communs/Footer';
 import HeroSection from '../components/sections/HeroSection';
 import PokemonPopulairesSection from '../components/sections/PokemonPopulairesSection';
@@ -6,22 +6,30 @@ import TypesPokemonSection from '../components/sections/TypesPokemonSection';
 import FonctionnalitésSections from '../components/sections/FonctionnalitésSections';
 import AppelActions from '../components/sections/AppelActions';
 
-function LandingPage(){
-    return(
-      <>
-        <div className="min-h-screen bg-gradient-to-br from-blue-50 via-white to-red-50 dark:from-slate-900 dark:via-slate-800 dark:to-slate-900 text-white">
-          <Header />          
-          <main>
-            <section id="landingpage"><HeroSection /></section>
-            <PokemonPopulairesSection />
-            <section id="types"><TypesPokemonSection/></section>
-           <section id="fonctionnalite"> <FonctionnalitésSections /></section>
-            <AppelActions />
-          </main>
-          <Footer />
-        </div>
-      </>
-    )
-};
+import SectionWithScrollAnimation from '../Assets/AnimationScrool';
 
-export default LandingPage
+function LandingPage() {
+  return (
+    <>
+      <div className="min-h-screen bg-gradient-to-br from-blue-50 via-white to-red-50 dark:from-slate-900 dark:via-slate-800 dark:to-slate-900 text-white">
+        <main>
+          <section id="landingpage"><HeroSection /></section>
+          <PokemonPopulairesSection />
+
+          <SectionWithScrollAnimation id="types">
+            <TypesPokemonSection />
+          </SectionWithScrollAnimation>
+
+          <SectionWithScrollAnimation id="fonctionnalite">
+            <FonctionnalitésSections />
+          </SectionWithScrollAnimation>
+
+          <AppelActions />
+        </main>
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+export default LandingPage;
